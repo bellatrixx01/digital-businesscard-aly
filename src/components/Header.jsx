0f8add1881941cb0ee1c437c4bb2b729a36efcc3
bellatrixx01 <93028359+bellatrixx01@users.noreply.github.com>
2022-09-14 14:04:58 +0800

    commit
    replace to public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from "react"
 export default function Header() {
   return (
     <header>
-      <img src="public/images/me2x2.jpg" alt="prof" width="317px" />
+      <img src="/images/profile.jpg" alt="prof" width="317px" />
+
       <h2 className="header--name">Alyssa Sanchez</h2>
       <p className="header--job">Frontend Developer</p>
       <p className="header--website">alyxnchz.website</p>
